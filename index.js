f7417b18f129708b2d49ae021ef9c3276ea37217
Al Amin <alaminhossain2000and@gmail.com>
2022-11-02 11:20:19 +0600

    vercel.json is added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.li11u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://trip-tuck:7sNdJlGKJnISbITs@cluster0.li11u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
